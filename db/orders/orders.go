61950991a48859ba61f7d88ab48c6ed64573ca24
Roman Dontsov <rodo@ciklum.com>
2019-06-12 17:45:57 +0300

    Add time to parse Order function

--- db/orders/orders.go
+++ db/orders/orders.go
@@ -58,6 +58,6 @@ func parseRows(rows *sql.Rows) []Order {
 }
 func parseRow(row *sql.Rows) (Order, error) {
 	p := Order{}
-	err := row.Scan(&p.ID, &p.UserID, &p.Description, &p.Status, &p.CreatedAt, &p.UpdatedAt, &p.DeletedAt, &p.UserID, &p.Prices)
+	err := row.Scan(&p.ID, &p.UserID, &p.Description, &p.Time, &p.Status, &p.CreatedAt, &p.UpdatedAt, &p.DeletedAt, &p.UserID, &p.Prices)
 	return p, err
 }
